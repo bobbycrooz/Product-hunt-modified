a2d2affc4fb299c806957c05e39a748814e3865b
bobbycrooz <idrisloove@gmail.com>
2021-03-29 15:40:40 -0700

    update design

--- src/components/Sidebar/SoonToLunch.jsx
+++ src/components/Sidebar/SoonToLunch.jsx
@@ -6,7 +6,6 @@ import ProductCardSide from '../Products/ProductCardSide'
 
 const Container = Styled.div`
     width: 300px;
-    border:1px solid red;
     display: flex;
     flex-direction:column;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
